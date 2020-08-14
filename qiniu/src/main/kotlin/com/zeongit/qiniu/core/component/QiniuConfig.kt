fe59174b2851e687409b6bb8688a2e4ab9d12002
245142637@qq.com <asd+-*/00>
2020-08-14 18:06:23 +0800

    七牛

---
+++ qiniu/src/main/kotlin/com/zeongit/qiniu/core/component/QiniuConfig.kt
@@ -0,0 +1,29 @@
+package com.zeongit.qiniu.core.component
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+
+/**
+ * @author fjj
+ * 配置
+ */
+@Component
+@ConfigurationProperties("qiniu")
+class QiniuConfig {
+    lateinit var qiniuAccessKey: String
+
+    lateinit var qiniuSecretKey: String
+    //临时储存空间
+    lateinit var qiniuTemporaryBucket: String
+    //头像储存空间
+    lateinit var qiniuAvatarBucket: String
+    //背景储存空间
+    lateinit var qiniuBackgroundBucket: String
+    //临时储存空间
+    lateinit var qiniuTemporaryBucketUrl: String
+    //头像储存空间
+    lateinit var qiniuAvatarBucketUrl: String
+    //背景储存空间
+    lateinit var qiniuBackgroundBucketUrl: String
+}
---
+++ web/src/main/kotlin/com/zeongit/web/controller/QiniuController.kt
@@ -0,0 +1,25 @@
+package com.zeongit.web.controller
+
+import com.zeongit.qiniu.core.component.QiniuConfig
+import com.zeongit.qiniu.core.util.QiniuAuth
+import com.zeongit.share.annotations.Auth
+import com.zeongit.share.annotations.RestfulPack
+import com.zeongit.share.model.Result
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("qiniu")
+class QiniuController(private val qiniuConfig: QiniuConfig) {
+    @Auth()
+    @GetMapping("getUploadToken")
+    @RestfulPack
+    fun get(): Result<String> {
+        val auth = QiniuAuth.create(qiniuConfig.qiniuAccessKey, qiniuConfig.qiniuSecretKey)
+        return Result(200, "", auth.uploadToken(qiniuConfig.qiniuTemporaryBucket))
+    }
+}
+
+
